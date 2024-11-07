481c00951e859628d5c2fa43ae340768e77494fd
Juan Plaza <jufeplasa@hotmail.com>
2024-11-07 14:07:58 -0500

    add connection to database

--- myapp/client/src/main/java/Client.java
+++ myapp/client/src/main/java/Client.java
@@ -1,3 +1,3 @@
-public class Server {
+public class Client {
     
 }
--- myapp/server/src/main/java/Server.java
+++ myapp/server/src/main/java/Server.java
@@ -1,3 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Server {
-    
+    private static final String URL = "jdbc:postgresql://xhgrid2:5432/votaciones";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static void main(String[] args) {
+        String query = "SELECT * FROM departamento";
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa a la base de datos");
+            // Aquí puedes añadir el código para interactuar con la base de datos
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                // Procesa los resultados
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                System.out.println("ID: " + id + ", Nombre: " + nombre);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error al conectar a la base de datos");
+        }
+    }
 }
