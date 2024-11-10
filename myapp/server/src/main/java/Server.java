a8f88fc2087a4abc855c83178ebff3c3ac88abcc
Juan Plaza <jufeplasa@hotmail.com>
2024-11-10 16:58:04 -0500

    creat query for a document with return

--- myapp/server/src/main/java/Server.java
+++ myapp/server/src/main/java/Server.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,24 +11,17 @@ public class Server {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static Scanner scanner;
+    private static Connection connection = null;
+    private static PreparedStatement pstmt = null;
+    private static ResultSet rs = null;
     public static void main(String[] args) {
         scanner=new Scanner(System.in);
-        Connection connection = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String query = "SELECT * FROM departamento";
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexion exitosa a la base de datos");
             // Aquí puedes añadir el código para interactuar con la base de datos
-            stmt = connection.createStatement();
-            rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                // Procesa los resultados
-                int id = rs.getInt("id");
-                String nombre = rs.getString("nombre");
-                System.out.println("ID: " + id + ", Nombre: " + nombre);
-            }
+            menu();
+            
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Error al conectar a la base de datos");
@@ -36,7 +30,7 @@ public class Server {
             // Cerrar ResultSet, Statement y Connection en el bloque finally
             try {
                 if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
+                if (pstmt != null) pstmt.close();
                 if (connection != null) connection.close();
                 System.out.println("Conexión cerrada correctamente");
             } catch (SQLException e) {
@@ -51,11 +45,14 @@ public class Server {
         do {
             showMenu();
             option=scanner.nextInt();
-            if (option==exitOption) {
+            scanner.nextLine();
+            if (option!=exitOption) {
                 doOptions(option);
             }
-        }while (option==exitOption);  
+        }while (option!=exitOption);  
     }
+
+
 
     public static void doOptions(int option){
         switch (option) {
@@ -63,8 +60,13 @@ public class Server {
                 System.out.println("Implementacion de muchas consultas por un archivo");
                 break;
             case 2:
-                System.out.println("Devolver el puesto de votacion por una cedula");
+                System.out.println("Ingresa un numero de cedula ");
+                int documento=scanner.nextInt();
+                findInfoById(documento);
                 break;
+            default :
+                System.out.println("Opcion incorrecta");
+            break;
         }
     }
 
@@ -77,6 +79,38 @@ public class Server {
         System.out.println("2: Consulte su puesto de votacion con su cedula");
         System.out.println("3: Salir de la aplicacion");
     }
+
+
+
+    public static void findInfoById(int documento) {
+        String query = "SELECT c.nombre, m.consecutive AS mesa, p.direccion, mu.nombre AS municipio, d.nombre AS departamento " +
+                    "FROM Ciudadano c " +
+                    "JOIN mesa_votacion m ON c.mesa_id = m.id " +
+                    "JOIN puesto_votacion p ON m.puesto_id = p.id " +
+                    "JOIN municipio mu ON p.municipio_id = mu.id " +
+                    "JOIN departamento d ON mu.departamento_id = d.id " +
+                    "WHERE c.documento = ?::text";
+        try {
+            pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, documento);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String nombre = rs.getString("nombre");
+                int mesa = rs.getInt("mesa");
+                String direccion = rs.getString("direccion");
+                String municipio = rs.getString("municipio");
+                String departamento = rs.getString("departamento");
+                System.out.println("Nombre: " + nombre + "\nMesa: " + mesa + "\nDirección: " + direccion + "\nMunicipio: " + municipio + "\nDepartamento: " + departamento);
+            } else {
+                System.out.println("No se encontraron resultados para el documento: " + documento);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error en consulta");
+        }
+    }
+
+
 
     /*
      * Metodo que calcula los factores
