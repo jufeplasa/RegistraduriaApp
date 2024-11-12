3fd9e0f57dbd52e145f947e33357d65b85f093da
Juan Plaza <jufeplasa@hotmail.com>
2024-11-12 14:46:43 -0500

    add consulta class

--- myapp/server/src/main/java/Server.java
+++ myapp/server/src/main/java/Server.java
@@ -1,41 +1,19 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.util.Scanner;
 
 public class Server {
-    private static final String URL = "jdbc:postgresql://xhgrid2:5432/votaciones";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static Consulta consulta;
     private static Scanner scanner;
-    private static Connection connection = null;
-    private static PreparedStatement pstmt = null;
-    private static ResultSet rs = null;
     public static void main(String[] args) {
         scanner=new Scanner(System.in);
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            consulta = new Consulta();
             System.out.println("Conexion exitosa a la base de datos");
-            // Aquí puedes añadir el código para interactuar con la base de datos
             menu();
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Error al conectar a la base de datos");
         }
         finally {
-            // Cerrar ResultSet, Statement y Connection en el bloque finally
-            try {
-                if (rs != null) rs.close();
-                if (pstmt != null) pstmt.close();
-                if (connection != null) connection.close();
-                System.out.println("Conexión cerrada correctamente");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            consulta.disconect();
+            System.out.println("Desconexion base de datos");
         }
     }
 
@@ -62,7 +40,7 @@ public class Server {
             case 2:
                 System.out.println("Ingresa un numero de cedula ");
                 int documento=scanner.nextInt();
-                findInfoById(documento);
+                consulta.findInfoById(documento);
                 break;
             default :
                 System.out.println("Opcion incorrecta");
@@ -79,37 +57,6 @@ public class Server {
         System.out.println("2: Consulte su puesto de votacion con su cedula");
         System.out.println("3: Salir de la aplicacion");
     }
-
-
-
-    public static void findInfoById(int documento) {
-        String query = "SELECT c.nombre, m.consecutive AS mesa, p.direccion, mu.nombre AS municipio, d.nombre AS departamento " +
-                    "FROM Ciudadano c " +
-                    "JOIN mesa_votacion m ON c.mesa_id = m.id " +
-                    "JOIN puesto_votacion p ON m.puesto_id = p.id " +
-                    "JOIN municipio mu ON p.municipio_id = mu.id " +
-                    "JOIN departamento d ON mu.departamento_id = d.id " +
-                    "WHERE c.documento = ?::text";
-        try {
-            pstmt = connection.prepareStatement(query);
-            pstmt.setInt(1, documento);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                String nombre = rs.getString("nombre");
-                int mesa = rs.getInt("mesa");
-                String direccion = rs.getString("direccion");
-                String municipio = rs.getString("municipio");
-                String departamento = rs.getString("departamento");
-                System.out.println("Nombre: " + nombre + "\nMesa: " + mesa + "\nDirección: " + direccion + "\nMunicipio: " + municipio + "\nDepartamento: " + departamento);
-            } else {
-                System.out.println("No se encontraron resultados para el documento: " + documento);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Error en consulta");
-        }
-    }
-
 
 
     /*
